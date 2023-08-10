05e07020e72c8c3127d779efe4e723784a28553e
Jonah Wagenveld <wagenveld@pc002.mpifr-bonn.mpg.de>
2023-08-10 17:59:22 +0200

    Fixed image trimming and add WSClean MFS primary beam correction

--- mkat_widebandpbcor.py
+++ mkat_widebandpbcor.py
@@ -61,52 +61,31 @@ def makeXYDegreesDistanceMaps(image, wcs, RADeg, decDeg):
 
     return xDegreesMap, yDegreesMap
 
-def trim_image(trimg, wcs, freqMHz, band, thresh, trim):
+def trim_image(trimg, wcs, beam_model, band, thresh, trim):
 
-    try:
-        beam = JimBeam('MKAT-AA-%s-JIM-2020' % band.upper())
-    except ValueError:
-        print('Band selected not recognized, check input carefully')
-        sys.exit()
-
-    shape = trimg[0].data[0, 0].shape
-    RADeg, decDeg = wcs.getCentreWCSCoords()
-    xDegMap, yDegMap = makeXYDegreesDistanceMaps(np.ones(shape, dtype = np.float64), wcs, RADeg, decDeg)
-
-    central_I = beam.I(xDegMap, yDegMap, freqMHz)
-    trimg[0].data[0,0][central_I < thresh] = np.nan
-
+    trimg[0].data[0,0][beam_model < thresh] = np.nan
     if trim:
         # Assumes N is at the top, E at the left
-        y, x = np.where(central_I >= thresh)
+        y, x = np.where(beam_model >= thresh)
         yMin, yMax = y.min(), y.max()
         xMin, xMax = x.min(), x.max()
-        _, decMin = wcs.pix2wcs(xMax, yMin)
-        _, decMax = wcs.pix2wcs(xMin, yMax)
-        decMinMax = np.array([decMin, decMax])
-        yDecAbsMax = np.array([yMin, yMax])[np.argmax(abs(decMinMax))]
-        RAMin, _ = wcs.pix2wcs(xMax, yDecAbsMax)
-        RAMax, _ = wcs.pix2wcs(xMin, yDecAbsMax)
-        clipDict = astImages.clipUsingRADecCoords(trimg[0].data[0, 0], wcs, RAMin, RAMax, decMin, decMax)
-        trimg[0].data = np.zeros([1, 1, clipDict['data'].shape[0], clipDict['data'].shape[1]])
-        trimg[0].data[0, 0] = clipDict['data']
 
-        for i in range(1, 3):
-            for k in WCSKeys:
-                trimg[0].header['%s%d' % (k, i)]=clipDict['wcs'].header['%s%d' % (k, i)]
+        clipped_image = trimg[0].data[0, 0, yMin:yMax, xMin:xMax]
+        trimg[0].data = np.zeros([1, 1, clipped_image.shape[0], clipped_image.shape[1]])
+        trimg[0].data[0, 0] = clipped_image
+
+        trimg[0].header['NAXIS1'] = clipped_image.shape[1]
+        trimg[0].header['NAXIS2'] = clipped_image.shape[0]
+        trimg[0].header['CRPIX1'] = int(clipped_image.shape[1]/2)
+        trimg[0].header['CRPIX2'] = int(clipped_image.shape[0]/2)
 
     return trimg
 
-def calculate_widebandpb(in_image, model, model_images, nterms, band, freqAxis, freqs, write_beams):
+def calculate_beams(image, model, model_images, band, freqAxis, freqs):
     '''
-    Calculate the wideband PB correction
+    Get beams at different frequencies
     '''
-    tt0_img = open_fits_casa(in_image+'.image.tt0')
-    cfreq = tt0_img[0].header['CRVAL'+str(freqAxis)]/1e6 #MHz
-    dfreq = tt0_img[0].header['CDELT'+str(freqAxis)]
-
-    temp_img = tt0_img
-    im_shape = temp_img[0].data[0, 0].shape
+    im_shape = image[0].data[0, 0].shape
 
     if model == 'katbeam':
         try:
@@ -115,12 +94,8 @@ def calculate_widebandpb(in_image, model, model_images, nterms, band, freqAxis, 
             print('Band selected not recognized, check input carefully')
             sys.exit()
 
-        if len(freqs) < nterms:
-            print('Number of frequencies is lower than number of Taylor terms, fit cannot be performed')
-            sys.exit()
-
         # Get coordinates from image header and create coordinate map
-        wcs = astWCS.WCS(tt0_img[0].header, mode='pyfits').copy()
+        wcs = astWCS.WCS(image[0].header, mode='pyfits').copy()
         wcs.updateFromHeader()
         RADeg, decDeg = wcs.getCentreWCSCoords()
         xDegMap, yDegMap = makeXYDegreesDistanceMaps(np.ones(im_shape, dtype = np.float64), wcs, RADeg, decDeg)
@@ -138,7 +113,6 @@ def calculate_widebandpb(in_image, model, model_images, nterms, band, freqAxis, 
         for i, pb_im in enumerate(pb_images):
             imagedata = open_fits_casa(pb_im)
             pbs[i,:] = imagedata[0].data[0,0].flatten()
-
             freqs[i] = float(pb_im.split('_')[-1].split('M')[0])
 
     if model == 'holo':
@@ -157,75 +131,32 @@ def calculate_widebandpb(in_image, model, model_images, nterms, band, freqAxis, 
     freqs = freqs[sorted_freq]
     pbs = pbs[sorted_freq,:]
 
-    y = pbs
-    x = (freqs-cfreq)/cfreq
-    pb_polyfit = np.polynomial.polynomial.polyfit(x, y, deg=nterms-1)
+    return pbs, freqs
 
-    # Renormalize tt0 to correct any small errors
-    pb_polyfit[0,:] /= np.nanmax(pb_polyfit[0,:])
-
-    if write_beams:
-        # Write PB images
-        temp_img[0].data[0,0,:,:] = pb_polyfit[0,:].reshape(im_shape[0], im_shape[1])
-        temp_img.writeto('pb_tt0.fits', overwrite=True)
-        if nterms > 1:
-            temp_img[0].data[0,0,:,:] = pb_polyfit[1,:].reshape(im_shape[0], im_shape[1])
-            temp_img.writeto('pb_tt1.fits', overwrite=True)
-
-            temp_img[0].data[0,0,:,:] = (pb_polyfit[1,:].reshape(im_shape[0], im_shape[1])
-                                        / pb_polyfit[0,:].reshape(im_shape[0], im_shape[1]))
-            temp_img.writeto('pb_alpha.fits', overwrite=True)
-
-    return pb_polyfit[:nterms,:]
-
-def main():
-
-    parser = new_argument_parser()
-    args = parser.parse_args()
-
-    in_image = args.image_name
-    model = args.model
-    model_images = args.model_images
-    nterms = args.nterms
-    write_beams = args.write_beams
-    freqs = args.freqs
-    band = args.band
-    thresh = args.thresh
-    trim = args.trim
-    alpha_thresh = args.alpha_thresh
-
-    # Open tt0 image for header information
+def casa_widebandpbcor(in_image, model, model_images, nterms, band, freqAxis, freqs, thresh, trim, write_beams):
+    '''
+    Calculate the wideband PB correction for CASA images
+    '''
     tt0_img = open_fits_casa(in_image+'.image.tt0')
-
-    # Determine frequency axis
-    # Order depends on if e.g. CASA or WSClean image
-    polAxis = None
-    freqAxis = None
-    for i in range(1, 5):
-        if tt0_img[0].header['CTYPE%d' % (i)] == 'STOKES':
-            polAxis=i
-        elif tt0_img[0].header['CTYPE%d' % (i)] == 'FREQ':
-            freqAxis=i
-    assert(polAxis is not None and freqAxis is not None)
-    freqMHz = tt0_img[0].header['CRVAL%d' % (freqAxis)]/1e6
-
     wcs = astWCS.WCS(tt0_img[0].header, mode='pyfits').copy()
     wcs.updateFromHeader()
 
-    print("Image central Frequency = %.3f MHz" % (freqMHz))
-    print("Selected %s-band" % band.upper())
+    temp_img = tt0_img
+    cfreq = tt0_img[0].header['CRVAL'+str(freqAxis)]/1e6 #MHz
 
-    prefix = "pbcorr"
-    outDir=os.path.split(os.path.abspath(in_image))[0]
-    fileBaseName = os.path.basename(in_image).split('.')[0]
+    # Get primary beam models
+    pbs, freqs = calculate_beams(tt0_img, model, model_images,
+                                 band, freqAxis, freqs)
 
-    # Calculate wideband Taylor terms
-    pb_tt = calculate_widebandpb(in_image, model, model_images, nterms, band, freqAxis, freqs, write_beams)
+    y = pbs
+    x = (freqs-cfreq)/cfreq
 
-    tt0_img = open_fits_casa(in_image+'.image.tt0')
-    pb_tt0 = pb_tt[0,:].reshape(tt0_img[0].data.shape[2],tt0_img[0].data.shape[3])
+    pb_polyfit = np.polynomial.polynomial.polyfit(x, y, deg=nterms-1)
+    # Renormalize tt0 to correct any small errors
+    pb_polyfit[0,:] /= np.nanmax(pb_polyfit[0,:])
 
-    # Apply PB correction
+    # Apply PB for Taylor term 0 image
+    pb_tt0 = pb_polyfit[0,:].reshape(tt0_img[0].data.shape[2],tt0_img[0].data.shape[3])
     tt0_img[0].data[0,0,:,:] = tt0_img[0].data[0,0,:,:]/pb_tt0
     tt0_untrimmed = copy.deepcopy(tt0_img)
 
@@ -235,13 +166,16 @@ def main():
     else:
         tt0_img[0].header['PBCOR'] = model
 
-    tt0_img = trim_image(tt0_img, wcs, freqMHz, band, thresh, trim)
-    outFileName = os.path.join(outDir,fileBaseName+"_"+prefix+"_tt0.fits")
-    tt0_img.writeto(outFileName, overwrite = True)
+    tt0_img = trim_image(tt0_img, wcs, pb_tt0, band, thresh, trim)
+    tt0_img.writeto(in_image+'_pbcorr_tt0.fits', overwrite = True)
+    if write_beams:
+        # Write PB images
+        temp_img[0].data[0,0,:,:] = pb_tt0
+        temp_img.writeto('pb_tt0.fits', overwrite=True)
 
     if nterms > 1:
         tt1_img = open_fits_casa(in_image+'.image.tt1')
-        pb_tt1 = pb_tt[1,:].reshape(tt1_img[0].data.shape[2],tt1_img[0].data.shape[3])
+        pb_tt1 = pb_polyfit[1,:].reshape(tt1_img[0].data.shape[2],tt1_img[0].data.shape[3])
 
         #Apply PB correction
         tt1_img[0].data[0,0,:,:] = (tt1_img[0].data[0,0,:,:] - pb_tt1*tt0_untrimmed[0].data[0,0,:,:])/pb_tt0
@@ -259,16 +193,133 @@ def main():
         else:
             tt0_untrimmed[0].header['PBCOR'] = model
 
-        tt0_untrimmed = trim_image(tt0_untrimmed, wcs, freqMHz, band, thresh, trim)
-        outFileName = os.path.join(outDir,fileBaseName+"_"+prefix+"_alpha.fits")
-        tt0_untrimmed.writeto(outFileName, overwrite = True)
+        tt0_untrimmed = trim_image(tt0_untrimmed, wcs, pb_tt0, band, thresh, trim)
+        tt0_untrimmed.writeto(in_image+'_pbcorr_alpha.fits', overwrite = True)
+
+        if write_beams:
+            temp_img[0].data[0,0,:,:] = pb_tt1
+            temp_img.writeto('pb_tt1.fits', overwrite=True)
+
+            temp_img[0].data[0,0,:,:] = pb_tt1 / pb_tt0
+            temp_img.writeto('pb_alpha.fits', overwrite=True)
+
+def wsclean_widebandpbcor(in_image, model, model_images, band, freqAxis, thresh, trim, write_beams):
+    '''
+    Calculate the wideband PB correction for WSClean images
+    '''
+    mfs_img = open_fits_casa(in_image+'-MFS-image.fits')
+    wcs = astWCS.WCS(mfs_img[0].header, mode='pyfits').copy()
+    wcs.updateFromHeader()
+
+    img_shape = np.squeeze(mfs_img[0].data).shape
+    temp_img = copy.deepcopy(mfs_img)
+
+    freqs = []
+    weights = []
+    spw_images = sorted(glob.glob(in_image+'-0*-image.fits'))
+    for spw_im_file in spw_images:
+        spw_im = open_fits_casa(spw_im_file)
+
+        freq = spw_im[0].header['CRVAL3']/1e6 #MHz
+        weight = 1/spw_im[0].header['WSCIMGWG']*1e9
+
+        print(freq, weight)
+
+        freqs.append(freq)
+        weights.append(weight)
+
+    # Get primary beam models
+    pbs, freqs = calculate_beams(mfs_img, model, model_images,
+                                 band, freqAxis, freqs)
+
+    mfs_pb = np.average(pbs, axis=0, weights=weights)
+    mfs_pb = mfs_pb.reshape(img_shape)
+    mfs_img[0].data[0,0,:,:] = mfs_img[0].data[0,0,:,:]/mfs_pb
+
+    # Add beam model to header and write to file
+    if model == 'katbeam':
+        mfs_img[0].header['PBCOR'] = 'katbeam-'+katbeam.__version__
+    else:
+        mfs_img[0].header['PBCOR'] = model
+    mfs_img = trim_image(mfs_img, wcs, mfs_pb, band, thresh, trim)
+    mfs_img.writeto(in_image+'-MFS-pbcor-image.fits', overwrite = True)
+
+    if write_beams:
+        # Write PB images
+        temp_img[0].data[0,0,:,:] = mfs_pb
+        temp_img.writeto('mfs_pb.fits', overwrite=True)
+
+def main():
+
+    parser = new_argument_parser()
+    args = parser.parse_args()
+
+    in_image = args.image_name
+    wsclean_mfs = args.wsclean_mfs
+    model = args.model
+    model_images = args.model_images
+    nterms = args.nterms
+    write_beams = args.write_beams
+    freqs = args.freqs
+    band = args.band
+    thresh = args.thresh
+    trim = args.trim
+    alpha_thresh = args.alpha_thresh
+
+    # Open mfs image for header information
+    if wsclean_mfs:
+        mfs_image_file = in_image+'-MFS-image.fits'
+    else:
+        mfs_image_file = in_image+'.image.tt0'
+
+    if os.path.isfile(mfs_image_file):
+        mfs_img = open_fits_casa(mfs_image_file)
+    else:
+        print(f'Input image {mfs_image_file} not found')
+        sys.exit(0)
+
+    # Determine frequency axis
+    # Order depends on if e.g. CASA or WSClean image
+    polAxis = None
+    freqAxis = None
+    for i in range(1, 5):
+        if mfs_img[0].header['CTYPE%d' % (i)] == 'STOKES':
+            polAxis=i
+        elif mfs_img[0].header['CTYPE%d' % (i)] == 'FREQ':
+            freqAxis=i
+    assert(polAxis is not None and freqAxis is not None)
+    freqMHz = mfs_img[0].header['CRVAL%d' % (freqAxis)]/1e6
+
+    wcs = astWCS.WCS(mfs_img[0].header, mode='pyfits').copy()
+    wcs.updateFromHeader()
+
+    print("Image central Frequency = %.3f MHz" % (freqMHz))
+    print("Selected %s-band" % band.upper())
+
+    if wsclean_mfs:
+        wsclean_widebandpbcor(in_image, model, model_images, 
+                              band, freqAxis, thresh, trim, write_beams)
+    else:
+        if model == 'katbeam' and len(freqs) < nterms:
+            print('Number of frequencies is lower than number of Taylor terms, fit cannot be performed')
+            sys.exit()
+        casa_widebandpbcor(in_image, model, model_images, 
+                           nterms, band, freqAxis, freqs, 
+                           thresh, trim,  write_beams)
 
 def new_argument_parser():
 
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("image_name", type=str, help="""An image to correct. Input assumes 
-                        CASA file structure, i.e. image_name.image.tt0, image_name.image.tt1, etc.""")
+    parser.add_argument("image_name", type=str, help="""An image to correct. Standard input assumes 
+                        CASA file structure, i.e. image_name.image.tt0, image_name.image.tt1.
+                        If wsclean-mfs is set to true, wsclean output will be assumed, i.e. 
+                        imagename-0000-image.fits, imagename-0001-image.fits for channel images
+                        and imagename-MFS-image.fits for the mfs image. In this case frequencies 
+                        will be obtained from the channel images and the freqs option ignored.""")
+    parser.add_argument("--wsclean_mfs", action='store_true',
+                        help="""Assume wsclean mfs weighting scheme instead of Taylor term images.
+                                Currently only works with katbeam model.""")
     parser.add_argument("--model", type=str, default="katbeam",
                         help="""Which primary beam model to use, options are katbeam, plumber, 
                                 and holo(graphic) (default=katbeam).""")
